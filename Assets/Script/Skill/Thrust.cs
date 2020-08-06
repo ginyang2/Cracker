622c4ebcc00908841473c1866310e995eb2484bb
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2020-08-06 20:55:37 +0900

    추가 : Thrust 스킬추가
    찌르기 스킬인 Thrust 스킬을 추가했다. 추후에 그래픽을 넣고, 범위를 손보고, 범위 바깥을 클릭해도 방향으로 근접이 나가도록 바꿔야 한다.

---
+++ Assets/Script/Skill/Thrust.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : Skill
+{
+    public float damage;
+    public GameObject attack;
+    public override IEnumerator UseSkill(Player player, Vector3 target)
+    {
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+        StartCoroutine(base.UseSkill(player));
+        
+        float angle = Mathf.Atan2(target.y - playerPos.y, target.x - playerPos.x) * Mathf.Rad2Deg;
+        angle -= 45;
+        GameObject made = Instantiate(attack, playerPos, Quaternion.identity);
+        made.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        ShortAttack longAttack = made.GetComponent<ShortAttack>();
+        longAttack.damage = (int)damage;
+        yield return new WaitForEndOfFrame();
+    }
+}
