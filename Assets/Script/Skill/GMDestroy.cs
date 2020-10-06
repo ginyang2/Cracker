29a7b8722800ed216c96d635114b9879eceb80f7
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2020-10-06 15:42:30 +0900

    추가 : GM용 몹쓸기 기술
    좀더 편한 테스트를위해 GM용 몹쓸기 기술을 만들었다.

---
+++ Assets/Script/Skill/GMDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMDestroy : Skill
+{
+    Enemy[] enemys;
+    public override IEnumerator UseSkill(Player player)
+    {
+        StartCoroutine(base.UseSkill(player));
+        enemys = FindObjectsOfType<Enemy>();
+        foreach (Enemy enemy in enemys)
+        {
+            enemy.MHP(999999);
+        }
+        yield return new WaitForEndOfFrame();
+    }
+}
